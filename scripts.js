0d5b80b9b1fd64f9ed1148a02d57e5a713cb0858
Karen Langner <krenabeth@yahoo.com>
2016-07-13 21:41:43 -0400

    appended Clicked! and added colors by id on each button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
-var $navBar = $('ul#navBar');
+// global variable for li
 var $li = $('li');
-//all li width of at least 200px
-$li.css('width', '400px');
-//all li height of at least 50px
-$li.css('height', '75px');
+var $navBar = $('.navBar');
+//all li width of at least 200px  // not working yet
+// //all li height of at least 50px
+$(function() {
+	$($navBar).css ({
+		'width': '200px',
+		'height': '75px'
+	});
+});
+// $('.navBar').css('width', '800px');
+
 // for each li, fade to 0.25 opacity for hover
 $li.hover(function() {
 	$( this ).fadeTo('slow', 0.25);	
@@ -15,19 +22,27 @@ $li.hover(function() {
 	$( this ).fadeTo('slow', 1);
 	}
 );
-//each li changes to 'Click!' when clicked
 
-//each li changes colors when clicked
-$( "li" ).addClass(function( index, currentClass ) {
-  var addedClass;
- 
-  if ( currentClass === "one" ) {
-    addedClass = "orange";
-  }
- 
-  return addedClass;
+// each li changes to 'Click!' when clicked
+// and first changes to orange
+$($('#one')).on('click', function() {
+	$( this ).append('Clicked!'),
+	$( this ).addClass('orange')
 });
-
-
+// second li to red
+$($('#two')).on('click', function() {
+	$( this ).append('Clicked!'),
+	$( this ).addClass('red')
+});
+//third to green
+$($('#three')).on('click', function() {
+	$( this ).append('Clicked!'),
+	$( this ).addClass('green')
+});
+//fourth to blue
+$($('#four')).on('click', function() {
+	$( this ).append('Clicked!'),
+	$( this ).addClass('blue')
+});
 
 }); // closing out document ready
